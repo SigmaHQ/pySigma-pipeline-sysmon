1ad86c773c55944179780f63fa7f6848c0335547
Thomas Patzke <thomas@patzke.org>
2023-02-06 00:58:24 +0100

    Updated to pySigma 0.9.0 and added plugin metadata

--- sigma/pipelines/sysmon/__init__.py
+++ sigma/pipelines/sysmon/__init__.py
@@ -1,1 +1,7 @@
+from typing import Callable, Dict
 from .sysmon import sysmon_pipeline
+from sigma.processing.pipeline import ProcessingPipeline
+
+pipelines : Dict[str, Callable[[], ProcessingPipeline]] = {
+    "sysmon": sysmon_pipeline,
+}
